9e7b988863afd1cd92c19118d0b467a836c3acb6
github username <github email address>
2015-09-02 15:05:00 -0400

    sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
 		user = User.find_by(name: params[:name])
 		if user && user.authenticate(params[:password])
 			session[:user_id] = user.id
-			redirect_to(posts_path)
+			redirect_to :controller => "users", :action => "edit", :id => user.id
 		else
 			flash[:error] = "Incorrect user Name or Password"
 			redirect_to(login_path)
